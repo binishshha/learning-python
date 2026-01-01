c382c024eadc05b889926d11155335fc9293243a
binishshha <binishabasnet299@gmail.com>
2026-01-01 16:56:01 +0545

    fixed upload fault

--- strings/stringMethods.py
+++ strings/stringMethods.py
@@ -5,16 +5,16 @@ print(message.find('h'))
 print(message.find("h",2,5))
 
 #TASK 1: Find position of first "a"
-# inp = input("Enter a string: ")
-# print(inp.find("a"))
+inp = input("Enter a string: ")
+print(inp.find("a"))
 
 # #TASK 2: Masking email address
-# email = input("Enter an email address: ")
-# symbol = email.find("@")
-# username = email[:symbol]
-# domain = email[symbol:]
-# maskedmail = username[0] + "*" * (len(username) - 2) + username[-1] + domain
-# print("Masked email:", maskedmail)
+email = input("Enter an email address: ")
+symbol = email.find("@")
+username = email[:symbol]
+domain = email[symbol:]
+maskedmail = username[0] + "*" * (len(username) - 2) + username[-1] + domain
+print("Masked email:", maskedmail)
 
 #.index() method locates the position of a string or substring
 print(message.index("man"))
