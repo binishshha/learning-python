f907bb12456d8a17f4949cda339355f5f1af68d1
binishshha <binishabasnet299@gmail.com>
2026-01-20 16:40:55 +0545

    task-completed

--- analysistask.py
+++ analysistask.py
@@ -26,13 +26,13 @@ for keys,values in Cities.items():
 
 # Answer the following questions using the data above:
 
-# 1. If you're planning a 1-week long trip, which city should you visit to spend the least amount of money?
-
 import math
 
 def total_cost(city,duration):
     values= Cities[city]
     return (values["Return_flight"]+values["Hotel_per_day"]*duration+values["weekly_car_rental"]*math.ceil(duration/7))
+
+# 1. If you're planning a 1-week long trip, which city should you visit to spend the least amount of money?
 
 def minimum_cost():
     total_costt={}
@@ -62,8 +62,10 @@ def minimum_cost1():
 
     for k in duration:
         total_cost1[k]={}
+        print(f"For a trip of duration {k} days:")
         for i in Cities.keys():
             total_cost1[k][i] = total_cost(i,k)
+        print(total_cost1[k])
     
     for k in duration:
         print(f"----for {k} days:---")
@@ -80,4 +82,67 @@ minimum_cost1()
 
 # 3. If your total budget for the trip is $1000, which city should you visit to maximize the duration of your trip? 
 # Which city should you visit if you want to minimize the duration?
+def maximize_duration(budget):
+    total_days={}
+
+    for city in Cities:
+        print(f"Total days you can spend in {city} in {budget}")
+        total_days[city]= round((budget-Cities[city]["Return_flight"])/(Cities[city]["Hotel_per_day"]+(Cities[city]["weekly_car_rental"]/7))) 
+        print(total_days[city])
+
+    maximum_duration=0
+
+    for i in total_days:
+        if total_days[i]>maximum_duration:
+            maximum_duration=total_days[i]
+            cityname=i
+    print(f"The maximum duration you can spend with budget {budget} is {maximum_duration} days at {cityname}")
+
+    minimum_duration=math.inf
+
+    for i in total_days:
+        if total_days[i]<minimum_duration:
+            minimum_duration=total_days[i]
+            cityname=i
+    print(f"The minimum duration you can spend with budget {budget} is {minimum_duration} days at {cityname}")
+            
+            
+maximize_duration(1000)
+
 # 4. How does the answer to the previous question change if your budget is $600, $2000 or $1500 ?
+def maximize_duration():
+    budget=[600, 2000, 1500]
+    total_days={}
+
+    for i in budget:
+        total_days[i]={}
+        print(f"Total days you can spend in budget {i} in different cities")
+        for city in Cities:
+            total_days[i][city]= round((i-Cities[city]["Return_flight"])/(Cities[city]["Hotel_per_day"]+(Cities[city]["weekly_car_rental"]/7))) 
+        print(total_days[i])
+
+    cityname=[]
+
+    for k in budget:
+        maximum_duration=0
+        for i in total_days[k]:
+            if total_days[k][i]>maximum_duration:
+                maximum_duration=total_days[k][i]
+                cityname=[i]
+            elif total_days[k][i]==maximum_duration:
+                cityname.append(i)
+        print(f"The maximum duration you can spend with budget {k} is {maximum_duration} days at {cityname}")
+        cityname.clear()
+
+    for k in budget:
+        minimum_duration=math.inf
+        for i in total_days[k]:
+            if total_days[k][i]<minimum_duration:
+                minimum_duration=total_days[k][i]
+                cityname=[i]
+            elif total_days[k][i]==minimum_duration:
+                cityname.append(i)
+        print(f"The miniimum duration you can spend with budget {k} is {minimum_duration} days at {cityname}")
+        cityname.clear()
+
+maximize_duration()
